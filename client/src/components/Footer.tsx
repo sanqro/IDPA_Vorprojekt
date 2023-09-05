55f0b3a518a2c83c1dcf1ef6831d7cf2812ce286
sandro <contact@sanqro.xyz>
2023-09-05 09:31:22 +0200

    add footer component
    Co-authored-by: h0peRL <nico.delvecchio@gmx.ch>

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-500 text-white p-4 absolute bottom-0 w-full text-center">
+      <a href="https://github.com/sanqro/IDPA_Vorprojekt" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
